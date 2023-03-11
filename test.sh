76f734a6fa20ad08c902655a3d2893ef51e623e0
Ryan Moore <ryanm.inbox@gmail.com>
2023-03-11 01:38:39 -0500

    fix 301 moved permanently error

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
-curl -X POST -H "Content-Type: application/json" -d '{"key": "apple", "value": "42"}' http://localhost:3333/cache
-curl -X POST -H "Content-Type: application/json" -d '{"key": "banana", "value": "666"}' http://localhost:3333/cache
+curl -X POST -H "Content-Type: application/json" -d '{"key": "apple", "value": "42"}' http://localhost:3333/cache/apple
+curl -X POST -H "Content-Type: application/json" -d '{"key": "banana", "value": "666"}' http://localhost:3333/cache/apple
 curl http://localhost:3333/cache/apple # get apple returns 42
 curl http://localhost:3333/cache/banana # get banana returns 666
-curl http://localhost:3333/cache # lists apple banana
+curl http://localhost:3333/cache/ # lists apple banana
